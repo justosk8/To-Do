b3bc7c15cf74506a6048bf272084160dd1653653
Ed Abbey <websterhomer78@gmail.com>
2021-03-08 20:37:17 -0700

    changed list to radio buttons. not working yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,26 +5,26 @@ var body = document.body;
 var unorderedList = document.querySelector('#list');
 var newTask = document.querySelector('#add-task');
 var submit = document.querySelector('button');
-var liEl = document.createElement('li');
-
+var liEl = document.createElement('input');
+var taskList = document.querySelectorAll('li');
 
 
 var addTask = function() {
     console.log(newTask.value);
-    var liEl = document.createElement('li');
+        
+    var liEl = document.createElement('input');
+    liEl.type = 'radio';
+
     unorderedList.appendChild(liEl);
-    liEl.textContent = newTask.value;
+    liEl.textContent  = newTask.value;
+
+    var label = document.createElement('label');
+    label.innerText = newTask.value;
+
     newTask.value = ""; 
     
 }
 
-var deleteTask = function() {
 
-}
-
-var saveTask = function() {
-    localStorage.setItem();
-    
-}
 
 submit.onclick = addTask;
