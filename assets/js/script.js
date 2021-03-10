8a2ce2831291fdb3a9b6924b626bb20dd36571d1
Ed Abbey <websterhomer78@gmail.com>
2021-03-09 19:20:07 -0700

    changed inputs to buttons, trying to get them to delete onclick

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,24 @@ var count = 0;
 var body = document.body;
 var unorderedList = document.querySelector('#list');
 var newTask = document.querySelector('#add-task');
-var submit = document.querySelector('button');
-var liEl = document.createElement('input');
-var taskList = document.querySelectorAll('li');
+var submit = document.querySelector('#submit-btn');
+var liEl = document.createElement('button');
+
 
 
 var addTask = function() {
     console.log(newTask.value);
         
-    var liEl = document.createElement('input');
-    liEl.type = 'radio';
-
+    var liEl = document.createElement('button');
     unorderedList.appendChild(liEl);
     liEl.textContent  = newTask.value;
-
-    var label = document.createElement('label');
-    label.innerText = newTask.value;
-
     newTask.value = ""; 
     
 }
 
-
+var deleteTask = function() {
+unorderedList.removeChild('button');
+}
 
 submit.onclick = addTask;
+liEl.onclick = deleteTask;
